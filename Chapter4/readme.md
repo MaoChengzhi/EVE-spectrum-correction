58f2818063c93db82ad620daa3b90d8810eabe4e
Chengzhi Mao <maochengzhi1@gmail.com>
2023-05-02 19:40:40 +0800

    split Chapter4 into  simple repetition and new trial , two parts
    download dozens of GB of EVE data

---
+++ Chapter4/readme.md
@@ -0,0 +1,1 @@
+simply repetition of Chapter 4 
---
+++ Chapter4_new/eve_part/gaussian_fit_eve.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.modeling import models, fitting
+import math
+
+# %%
+
+
+def gaussian_fit_eve(wavelength_list, irradiance):
+    '''
+    Fit a Gaussian function to the given irradiance data.
+
+    Parameters
+    ----------
+    irradiance : (12,) ndarray
+        A 1D array of irradiance data to fit the Gaussian function to.
+
+    Returns
+    -------
+    float
+        The mean value of the fitted Gaussian function.
+
+    Notes
+    -----
+    The function uses the astropy modeling and fitting library to perform the fitting. It sets the initial parameters of the Gaussian function to amplitude=1E9, mean=0.05, and stddev=0.0424, and uses the LevMarLSQFitter algorithm for fitting.
+
+    The function also imports a pre-defined list of wavelengths from the constant module, which is used to evaluate the fitted Gaussian function.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> x = np.linspace(-1, 1, 101)
+    >>> y = np.exp(-0.5*(x/0.1)**2)
+    >>> mean = gaussian_fit(y)
+    >>> print(mean)
+    0.0
+    '''
+    
+    if(np.sum(np.isnan(irradiance))>0 or (irradiance <= 0).any()):
+        return np.nan
+    
+    
+    
+    g_init = models.Gaussian1D(amplitude=0.005, mean=30.3, stddev=0.0424)
+
+    # initial value for fitting
+    fit_g = fitting.LevMarLSQFitter()
+    g = fit_g(g_init, wavelength_list, irradiance)
+    if(g.stddev>0.1):
+        print("unsuccessful fitting")
+        return np.nan
+    mean = g.mean.value
+
+    return mean
