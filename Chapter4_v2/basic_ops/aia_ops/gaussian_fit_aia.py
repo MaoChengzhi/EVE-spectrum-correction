b7ca95d383392958505e8a8c2ff4facaeb4a52f1
Chengzhi Mao <maochengzhi1@gmail.com>
2023-07-25 17:04:38 +0800

    AIA image & EVE para   correlation
    calculate MSE between images, and get a matrix.

---
+++ Chapter4_v2/basic_ops/aia_ops/gaussian_fit_aia.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cupy as cp
+import matplotlib.pyplot as plt
+from astropy.modeling import models, fitting
+import math
+
+# %%
+def gaussian_fit_aia(wavelength_list, irradiance):
+    '''
+    Fit a Gaussian function to the given irradiance data.
+
+    Parameters
+    ----------
+    irradiance : ndarray
+        A 1D array of irradiance data to fit the Gaussian function to.
+
+    Returns
+    -------
+    float
+        The mean value of the fitted Gaussian function.
+
+    Notes
+    -----
+    The function uses the astropy modeling and fitting library to perform the fitting. It sets the initial parameters of the Gaussian function to amplitude=1E9, mean=0.05, and stddev=0.0424, and uses the LevMarLSQFitter algorithm for fitting.
+
+
+    '''
+    
+    if(np.sum(np.isnan(irradiance))!=0):
+        return np.nan,np.nan,np.nan
+
+    try:
+        g_init = models.Gaussian1D(amplitude=1E9, mean=30.3783, stddev=0.03)
+        #
+        # initial value for fitting
+        fit_g = fitting.LevMarLSQFitter()
+        g = fit_g(g_init, wavelength_list, irradiance)
+        mean = g.mean.value
+        stddev=g.stddev.value
+        amplitude=g.amplitude.value
+        return mean,stddev,amplitude
+    except:
+        return np.nan,np.nan,np.nan
---
+++ Chapter4_v2/basic_ops/eve_ops/data/eve_const.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+wavelength_chianti = 30.3783
+wavelength_list_eve = np.array([30.27, 30.29, 30.31, 30.33, 30.35, 30.37,
+                                30.39, 30.41, 30.43, 30.45, 30.47, 30.49])
